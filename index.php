b07de6ba8372236fe7ab25ff7ce4bcafb1c1cda7
Calvin <Calvin@Calvin_Laptop>
2015-01-07 13:15:50 -0800

    Setup Homepage

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Comp4711 Lab1</title>
+    </head>
+    <body>
+        <?php
+        echo "Hello World!"
+        ?>
+    </body>
+</html>
